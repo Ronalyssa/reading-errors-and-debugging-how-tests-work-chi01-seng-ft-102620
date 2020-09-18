24249e223d084e459eddeb60d8923d9a46b02f46
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-18 16:30:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
 # Add your variables here
+first_number = 3
+
+second_number = 9
+
+sum = first_number + second_number
+
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = second_number / first_number
